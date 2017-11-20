7ffbcd6f9556d651352dec7745198b53e630dab7
Konstantin Stolyarenko <stolyarenko.kons@mail.ru>
2017-11-20 19:50:45 +0200

    Initial Commit

---
+++ ModelExampleForIS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModelExampleForIS
+//
+//  Created by Kostya on 20.11.2017.
+//  Copyright © 2017 SKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModelExampleForIS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModelExampleForIS
+//
+//  Created by Kostya on 20.11.2017.
+//  Copyright © 2017 SKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
